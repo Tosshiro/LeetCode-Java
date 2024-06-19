dbbb34b624e52db11303c8078355176c46f4c9fd
Tosshiro <hjianwei1@gmail.com>
2024-06-19 23:40:51 +0800

    Added problem 344

---
+++ 2. Two Pointers/344_Reverse_String.java
@@ -0,0 +1,24 @@
+'''
+Description
+LeetCode solution to 344. Reverse String
+
+Website Link: https://leetcode.com/problems/reverse-string/description/
+
+@author: Jw
+'''
+c
+lass Solution {
+    public void reverseString(char[] s) {
+        // Two Pointers to swap s[left] with s[right]
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
