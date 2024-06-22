8fe25f88ea6824e143cc1c3cb117028c45585661
Tosshiro <hjianwei1@gmail.com>
2024-06-22 12:08:50 +0800

    Added problem 881

---
+++ 2. Two Pointers/881_Boats_to_Save_People.java
@@ -0,0 +1,30 @@
+'''
+Description
+LeetCode solution to 881. Boats to Save People
+
+Website Link: https://leetcode.com/problems/boats-to-save-people/description/
+
+@author: JW
+'''
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        // Sorting and two pointer
+        Arrays.sort(people);
+        int counter = 0;
+        int left = 0;
+        int right = people.length - 1;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                // Both left pointer and right pointer person can take the boat
+                left++;
+                right--;
+            } else {
+                // Right pointer person takes a boat alone and shifts to the next lighter person 
+                right--;
+            }
+            counter++;
+        }
+        return counter;
+    }
+}
